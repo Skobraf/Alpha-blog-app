2c5c1f134808081b4958b056c7697b04eff28072
Ayoub <skobraf@gmail.com>
2018-06-24 17:56:32 +0100

    added show and create templates

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,24 @@
+class ArticlesController < ApplicationController
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save
+			flash[:notice] = "Your article was successfully created"
+			redirect_to @article
+		else
+			render :new
+		end
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
+	private
+	def article_params
+		params.require(:article).permit(:title,:description)
+	end
+end
