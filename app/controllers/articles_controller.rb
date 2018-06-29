2700382d83698158c1b2dfa8101885394b2b1528
Ayoub <skobraf@gmail.com>
2018-06-29 18:16:02 +0100

    fixed routes and updated create action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,7 +7,7 @@ class ArticlesController < ApplicationController
 		@article = Article.new(article_params)
 		if @article.save
 			flash[:notice] = "Your article was successfully created"
-			redirect_to @article
+			redirect_to article_path(@article)
 		else
 			render :new
 		end
