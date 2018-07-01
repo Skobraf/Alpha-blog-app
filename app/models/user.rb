e0bb5cfb14b1769053cf124ec438e0111011c206
Ayoub <skobraf@gmail.com>
2018-07-01 19:24:44 +0100

    added validations and user assosciation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_many :articles
 	before_save { self.email = email.downcase }
 	validates :username, presence: true,
 				 length: {minimum: 3, maximum: 25},
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_22_164259) do
+ActiveRecord::Schema.define(version: 2018_07_01_173532) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
