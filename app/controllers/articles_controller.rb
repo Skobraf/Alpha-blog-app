639a6c5cfc9a6ac681c3e1dab8822e93909b0dcb
Ayoub <skobraf@gmail.com>
2018-07-08 18:10:27 +0100

    added authentications restrictions on controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
 
 	before_action :set_article, only: [:edit, :update, :show, :destroy]
-
+	before_action :require_user, except: [:index, :show]
+	before_action :require_same_user, only: [:edit, :update, :destroy]
 	def index
 		@articles = Article.paginate(page: params[:page], per_page: 5)
 	end
@@ -12,7 +13,7 @@ class ArticlesController < ApplicationController
 
 	def create
 		@article = Article.new(article_params)
-		@article.user = User.first
+		@article.user = current_user
 		if @article.save
 			flash[:notice] = "Your article was successfully created"
 			redirect_to article_path(@article)
@@ -49,5 +50,10 @@ class ArticlesController < ApplicationController
 	end
 	def article_params
 		params.require(:article).permit(:title,:description)
+	end
+	def require_same_user
+		if current_user != @article.user
+			flash[:danger] = "You canonly delete or edit your article"
+			redirect_to root_path
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
 	before_action :set_user, only: [:edit, :update, :show]
-
+	before_action :require_same_user, only: [:edit, :update]
 	def new
 		@user = User.new
 	end
@@ -39,5 +39,11 @@ class UsersController < ApplicationController
 	end
 	def set_user
 		@user = User.find(params[:id])
+	end
+	def require_same_user
+		if current_user != @user
+			flash[:danger] = "You can only edit your own account"
+			redirect_to root_path
+		end
 	end
 end
