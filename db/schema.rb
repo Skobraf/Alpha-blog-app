e45f7e9bfa823726ca49113240906a66f27f2905
Ayoub <skobraf@gmail.com>
2018-07-11 16:57:28 +0100

    added admin privilige

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_04_111812) do
+ActiveRecord::Schema.define(version: 2018_07_11_153137) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_07_04_111812) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
