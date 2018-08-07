4b1ba3a7236d45fbabc703df3ea6c28d8d87d382
Ayoub <skobraf@gmail.com>
2018-08-07 15:20:50 +0100

    added test for Category model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_11_153137) do
+ActiveRecord::Schema.define(version: 2018_08_07_140549) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2018_07_11_153137) do
     t.datetime "updated_at"
     t.integer "user_id"
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
